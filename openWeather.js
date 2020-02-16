4292241253777f70870fee5a4755ec9ab86406b0
Ian <ian.shane.sears@gmail.com>
2020-02-16 16:35:09 -0700

    00.00.08 city on click trim and go to storage for recall to past cities list

--- openWeather.js
+++ openWeather.js
@@ -1,3 +1,5 @@
+// ----------------------------------------------------------------------------------------------------------------------------------------open weather temp/ wind GET
+
 $(document).ready(function(){
 
     $('#searchBtn').click(function(){
@@ -19,7 +21,40 @@ $(document).ready(function(){
                 console.log(data.main.temp_max)
                 console.log(data.main.humidity)
                 console.log(data.wind.speed)
-                console.log(data.wind)
+                localStorage.setItem("City Storage", city)
+            }
+        });
+    }
+    else {
+        alert('oof')
+    }
+    })
+})
+// ----------------------------------------------------------------------------------------------------------------------------------------open weather temp/ wind GET
+
+$(document).ready(function(){
+
+    $('#searchBtn').click(function(){
+
+    var city = $('#search').val();
+
+    if(city != ''){
+
+        $.ajax({
+
+            url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + 
+            "&APPID=1fdbaf1673606b82b778b52e97f9ce57",
+            type: "GET",
+            dataType: "jsonp",
+            success: function (data) {
+                console.log(data.list[0].main.temp)
+                // console.log(data.main.feels_like)
+                // console.log(data.main.temp_min)
+                // console.log(data.main.temp_max)
+                // console.log(data.main.humidity)
+                // console.log(data.wind.speed)
+                // console.log(data.wind)
+                
             }
         });
     }
