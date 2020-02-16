819f31e82b08b9f10034832ca27a37c2ceda9009
Ian <ian.shane.sears@gmail.com>
2020-02-16 16:09:14 -0700

    00.00.07 ajax call and weather data coming in and displaying in clg

---
+++ openWeather.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    $('#searchBtn').click(function(){
+
+    var city = $('#search').val();
+
+    if(city != ''){
+
+        $.ajax({
+
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + 
+            "&APPID=1fdbaf1673606b82b778b52e97f9ce57",
+            type: "GET",
+            dataType: "jsonp",
+            success: function (data) {
+                console.log(data.main.temp)
+                console.log(data.main.feels_like)
+                console.log(data.main.temp_min)
+                console.log(data.main.temp_max)
+                console.log(data.main.humidity)
+                console.log(data.wind.speed)
+                console.log(data.wind)
+            }
+        });
+    }
+    else {
+        alert('oof')
+    }
+    })
+})
