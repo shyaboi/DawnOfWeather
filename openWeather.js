e6a12c88cffd105828b08cfcb158c15a717e9bd8
e <e@lovelys-mbp.lan>
2020-03-11 18:33:42 -0700

    quality changes

--- openWeather.js
+++ openWeather.js
@@ -94,6 +94,7 @@ $(document).ready(function(){
                 $("#iconDayFiv").html("<img src='" + iconUrl  + "'>");
                 // weather icon list for 5 day----------------------------------------------------------------------------------------------------------------------------------------------------------
                 // weather temp and humidity list for 5 day----------------------------------------------------------------------------------------------------------------------------------------------------------
+                console.log(data.list[0].main.temp);
                 dayOneTemp = data.list[0].main.temp
                 dayTwoTemp = data.list[1].main.temp
                 dayThrTemp = data.list[2].main.temp
@@ -110,11 +111,11 @@ $(document).ready(function(){
                
 
                 // print 5 day in each 5 day forcast box-------------------------------------------------------------------------------------------------
-                $('#DayOnePrint').text('Temp'+' ' +temp+'F°').append('<br>'+'Hum:'+' '+ dayOneHumid+'%'+'</br>');
-                $('#DayTwoPrint').text('Temp'+' ' +temp+'F°').append('<br>'+'Hum:'+' '+ dayTwoHumid+'%'+'</br>');
-                $('#DayThrPrint').text('Temp'+' ' +temp+'F°').append('<br>'+'Hum:'+' '+ dayThrHumid+'%'+'</br>');
-                $('#DayFouPrint').text('Temp'+' ' +temp+'F°').append('<br>'+'Hum:'+' '+ dayFouHumid+'%'+'</br>');
-                $('#DayFivPrint').text('Temp'+' ' +temp+'F°').append('<br>'+'Hum:'+' '+ dayFivHumid+'%'+'</br>');
+                $('#DayOnePrint').text('Temp'+' ' +dayOneTemp+'F°').append('<br>'+'Hum:'+' '+ dayOneHumid+'%'+'</br>');
+                $('#DayTwoPrint').text('Temp'+' ' +dayTwoTemp+'F°').append('<br>'+'Hum:'+' '+ dayTwoHumid+'%'+'</br>');
+                $('#DayThrPrint').text('Temp'+' ' +dayThrTemp+'F°').append('<br>'+'Hum:'+' '+ dayThrHumid+'%'+'</br>');
+                $('#DayFouPrint').text('Temp'+' ' +dayFouTemp+'F°').append('<br>'+'Hum:'+' '+ dayFouHumid+'%'+'</br>');
+                $('#DayFivPrint').text('Temp'+' ' +dayFivTemp+'F°').append('<br>'+'Hum:'+' '+ dayFivHumid+'%'+'</br>');
                 
             }
         });
@@ -186,8 +187,8 @@ $(document).ready(function(){
     $('#pastCities li').click(function(e){
       let str = this.innerHTML;
       str = str.slice(0, -4); 
-      console.log(str);
-      alert(str);
+    //   console.log(str);
+    //   alert(str);
     var city = str
     $.ajax({
 
@@ -250,7 +251,62 @@ $(document).ready(function(){
             }
               });
             }
-            
+            $.ajax({
+                // ajax to open weather for 5 day---------------------------------------------------------------------------------------------------------
+                            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + 
+                            "&APPID=1fdbaf1673606b82b778b52e97f9ce57",
+                            type: "GET",
+                            dataType: "jsonp",
+                            // succsess data ------------------------------------------------------------------------------------------------------------------------------
+                            success: function (data) {
+                             
+                                // weather icon list for 5 day----------------------------------------------------------------------------------------------------------------------------------------------------------
+                                var iconCode = data.list[0].weather[0].icon;
+                                var iconCodeTwo = data.list[1].weather[0].icon;
+                                var iconCodeThr = data.list[2].weather[0].icon;
+                                var iconCodeFou = data.list[3].weather[0].icon;
+                                var iconCodeFiv = data.list[4].weather[0].icon;
+                
+                
+                                var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+                                var iconUrl = "http://openweathermap.org/img/w/" + iconCodeTwo + ".png";
+                                var iconUrl = "http://openweathermap.org/img/w/" + iconCodeThr + ".png";
+                                var iconUrl = "http://openweathermap.org/img/w/" + iconCodeFou + ".png";
+                                var iconUrl = "http://openweathermap.org/img/w/" + iconCodeFiv + ".png";
+                
+                
+                                $("#iconDayOne").html("<img src='" + iconUrl  + "'>");
+                                $("#iconDayTwo").html("<img src='" + iconUrl  + "'>");
+                                $("#iconDayThr").html("<img src='" + iconUrl  + "'>");
+                                $("#iconDayFou").html("<img src='" + iconUrl  + "'>");
+                                $("#iconDayFiv").html("<img src='" + iconUrl  + "'>");
+                                // weather icon list for 5 day----------------------------------------------------------------------------------------------------------------------------------------------------------
+                                // weather temp and humidity list for 5 day----------------------------------------------------------------------------------------------------------------------------------------------------------
+                                console.log(data.list[0].main.temp);
+                                dayOneTemp = data.list[0].main.temp
+                                dayTwoTemp = data.list[1].main.temp
+                                dayThrTemp = data.list[2].main.temp
+                                dayFouTemp = data.list[3].main.temp
+                                dayFivTemp = data.list[4].main.temp
+                
+                                dayOneHumid = data.list[0].main.humidity
+                                dayTwoHumid = data.list[1].main.humidity
+                                dayThrHumid = data.list[2].main.humidity
+                                dayFouHumid = data.list[3].main.humidity
+                                dayFivHumid = data.list[4].main.humidity
+                                // weather temp and humidity list for 5 day----------------------------------------------------------------------------------------------------------------------------------------------------------
+                
+                               
+                
+                                // print 5 day in each 5 day forcast box-------------------------------------------------------------------------------------------------
+                                $('#DayOnePrint').text('Temp'+' ' +dayOneTemp+'F°').append('<br>'+'Hum:'+' '+ dayOneHumid+'%'+'</br>');
+                                $('#DayTwoPrint').text('Temp'+' ' +dayTwoTemp+'F°').append('<br>'+'Hum:'+' '+ dayTwoHumid+'%'+'</br>');
+                                $('#DayThrPrint').text('Temp'+' ' +dayThrTemp+'F°').append('<br>'+'Hum:'+' '+ dayThrHumid+'%'+'</br>');
+                                $('#DayFouPrint').text('Temp'+' ' +dayFouTemp+'F°').append('<br>'+'Hum:'+' '+ dayFouHumid+'%'+'</br>');
+                                $('#DayFivPrint').text('Temp'+' ' +dayFivTemp+'F°').append('<br>'+'Hum:'+' '+ dayFivHumid+'%'+'</br>');
+                                
+                            }
+                        });
         }
     });
     })
